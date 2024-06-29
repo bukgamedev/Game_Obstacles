61ccdb69bbb88203844af13a72e02a2c8bcee800
bukgamedev <bukgamedev@gmail.com>
2024-06-29 20:37:11 +0300

    animation and code update
    I corrected the character's animations and wrote the code for the character's movements such as running and jumping.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,70 +5,66 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Animator Player_Animator; // Oyuncunun Animatörü
-    public Rigidbody Rigidbody; //Karakterin rigidbody'si
-    public float Walk_Speed, WalkBack_Speed, OldWalking_Speed, Run_Speed,BackRun_Speed, Rotate_Speed;
-    //Yürüme hýzý, geri yürüme hýzý, Eski yürüme hýzý, Koþma hýzý, Geri koþma hýzý, Dönüþ hýzý
-    public bool walking; //Karakter yürüyor mu?
-    public Transform PlayerTansform; //Karakterin transform deðeri
-    void FixedUpdate()
+    public float speed;//Karakterin koþma hýzý
+    public float rotation_Speed; //Dönme hýzý
+    public float Jump_Speed; //zýplama hýzý
+    public float JumpButtonGracePediod; //Zýplama hýzý süresi
+    public Animator animator; //Karakterin animator kontrolü
+    public CharacterController characterController; //Karakter kontrol component'ý
+    private float ySpeed; //Y eksenindeki hýzý
+    private float originalStepOffset; //Karakter kontrol component'ý içerisindeki ayar
+    private float? lastGroundedTÝme;
+    private float? JumpButtonPressedTime;
+
+    void Start()
     {
-        if (Input.GetKey(KeyCode.W)) //W tuþuna basýldýðýnda
-        {
-            Rigidbody.velocity = transform.forward * Walk_Speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            Rigidbody.velocity = -transform.forward * WalkBack_Speed * Time.deltaTime;
-        }
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset;
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) //W'a basýlý tutarsam yürümeeyi baþlat.
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
+        movementDirection.Normalize();
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+        if (characterController.isGrounded)
         {
-            Player_Animator.SetTrigger("Walk");
-            Player_Animator.ResetTrigger("Idle");
-            walking = true;
+            lastGroundedTÝme = Time.time;
         }
-        if (Input.GetKeyUp(KeyCode.W)) // Eðer W tuþundan elimi çekersem yürümeyi durdur.
+        if (Input.GetButtonDown("Jump"))
         {
-            Player_Animator.ResetTrigger("Walk");
-            Player_Animator.SetTrigger("Idle");
-            walking = false;
+            JumpButtonPressedTime = Time.time;
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Time.time - lastGroundedTÝme <= JumpButtonGracePediod)
         {
-            Player_Animator.SetTrigger("Walk_Back");
-            Player_Animator.ResetTrigger("Idle");
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            Player_Animator.ResetTrigger("Walk_Back");
-            Player_Animator.SetTrigger("Idle");
-        }
-        if (Input.GetKey(KeyCode.A)) //Klavyeden a tuþuna basýlýrsa
-        {
-            PlayerTansform.Rotate(0, -Rotate_Speed * Time.deltaTime,0); //Karakter sola doðru dönecektir.
-        } 
-        if (Input.GetKey(KeyCode.D))//Klavyeden d tuþuna basýlýrsa 
-        {
-            PlayerTansform.Rotate(0, Rotate_Speed * Time.deltaTime, 0); //Karakter saða doðru dönecektir.
-        }
-        if (walking == true)
-        {
-            if (Input.GetKeyDown(KeyCode.LeftShift)) // Sol Shift tuþuna basýlýrsa
+            characterController.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
+            if (Time.time - JumpButtonPressedTime <= JumpButtonGracePediod)
             {
-                Walk_Speed = Walk_Speed + Run_Speed; // Karakterin yürüme hýzýný, Yürüme hýzý ve Koþma hýzýnýn toplamýna eþitle. 
-                Player_Animator.SetTrigger("Run"); //Karakterin koþma animasyonunu oynat.
-                Player_Animator.ResetTrigger("Walk"); //Karakter koþacaðý için walk Animasyonunu resetle.
+                ySpeed = Jump_Speed;
+                JumpButtonPressedTime = null;
+                lastGroundedTÝme = null;
             }
-            if (Input.GetKeyUp(KeyCode.LeftShift)) //Sol Shift tuþundan parmak çekilirse
-            {
-                Walk_Speed = OldWalking_Speed; //Yürüme hýzýný, eski yürüme hýzýna eþitle.
-                Player_Animator.ResetTrigger("Run"); //Karakter koþmayý durduracaðý için, koþma animasyonunu resetle.
-                Player_Animator.SetTrigger("Walk"); ////Karakterin yürüme animasyonunu oynat.
-            }
+        }
+        else
+        {
+            characterController.stepOffset = 0;
+        }
+        Vector3 velocity = movementDirection * magnitude;
+        velocity.y = ySpeed;
+        characterController.Move(velocity * Time.deltaTime);
+        if (movementDirection != Vector3.zero)
+        {
+            animator.SetBool("IsRunning", true);
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotation_Speed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("IsRunning", false);
         }
     }
-
 }
