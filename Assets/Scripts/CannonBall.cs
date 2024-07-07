f36afcfc138bce5f4f93fd91bbb88ec38ccf37f1
bukgamedev <bukgamedev@gmail.com>
2024-07-07 23:39:20 +0300

    CannonBall Script  Updated.
    Things like latency, burst power etc were added to the code

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -4,21 +4,63 @@ using UnityEngine;
 
 public class CannonBall : MonoBehaviour
 {
-    public GameObject explosionEffectPrefab; // Patlama efekti prefabý
-    public float explosionRadius = 1f; // 2 metre çapýndaki çemberin yarýçapý
+    public GameObject ExplosionEffectPrefab; // Patlama efekti prefabý
     public LayerMask playerLayer; // Sadece oyuncuyu kontrol etmek için katman
+    public float delay = 1.0f;
+    public float radius = 5.0f;// 5 metre çapýndaki patlamanýn yarýçapý
+    public float force = 1500.0f; //Güllenin patlama gücü 
+    public bool ExplodeOnCollision = false;
+    public float DelayTimer; //gecikme 
+    public float EffectDisplayTime = 3.0f; //Efekt Görüntüleme Süresi
 
+    private void Awake()
+    {
+        DelayTimer = 0.0f;
+    }
+    private void Update()
+    {
+        DelayTimer += Time.deltaTime;
+        if (DelayTimer >= delay && !ExplodeOnCollision)
+        {
+            DoExplosion();
+            Destroy(gameObject);//Objeyi yok et.
+        }
+    }
+    private void DoExplosion()
+    {
+        HandleEffect();
+        HandleDestruction();
+    }
+    private void HandleEffect()
+    {
+        if (ExplosionEffectPrefab != null) //Explosioneffectprefab null deðilse
+        {
+            GameObject effect = Instantiate(ExplosionEffectPrefab, transform.position, Quaternion.identity);
+            Destroy(effect, EffectDisplayTime);
+        }
+        
+    }
+    private void HandleDestruction()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider collider in colliders)
+        {
+            Rigidbody rigidbody = collider.GetComponent<Rigidbody>();
+            if (rigidbody != null) //Rigidbody null deðilse,
+            {
+                rigidbody.AddExplosionForce(force, transform.position, radius); 
+            }
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
-
-        
         if (collision.gameObject.CompareTag("Ground"))// Yalnýzca Zemine düþtüðünde patlama efekti oynat
         {
             
-            Instantiate(explosionEffectPrefab, transform.position, Quaternion.identity);// Patlama efektini oluþtur
+            Instantiate(ExplosionEffectPrefab, transform.position, Quaternion.identity);// Patlama efektini oluþtur
 
             
-            Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius, playerLayer);// Çemberin içindeki karakteri kontrol et
+            Collider[] colliders = Physics.OverlapSphere(transform.position, radius, playerLayer);// Çemberin içindeki karakteri kontrol et
             foreach (Collider nearbyObject in colliders)
             {
                 if (nearbyObject.CompareTag("Player"))
