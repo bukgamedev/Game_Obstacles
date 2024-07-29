d46061d882882768a20fca35198e92ef0c1384ff
bukgamedev <bukgamedev@gmail.com>
2024-07-29 22:40:05 +0300

    Hook_Platform Successfully Hits the Other Platform and Returns.
    If it hits the object in the Hook_Platform Untagged tag, it returns to the point it came from and such a loop is formed.
    When necessary, I can also make the platform move and stop where it goes.

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -5,13 +5,37 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     public Rigidbody rb;
-    public float speed;
+    public float Speed;
+    public float Force_Speed;
+    public bool Timer;
     private void Start()
     {
-        
+        Force_Speed = Speed;
     }
-    private void Update()
+    private void FixedUpdate()
     {
-        rb.velocity = Vector3.forward * speed;
+        rb.velocity = Vector3.forward * Force_Speed;
+        if (Timer)
+        {
+            if (Force_Speed>0)
+            {
+                Force_Speed = -Speed; //Platform bir yere dokunduðunda - hýzda geriye doðru gidecek.
+                Timer = false; 
+            }
+            else if(Force_Speed < 0)
+            {
+                Force_Speed = Speed;
+                Timer = false;
+
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Untagged")
+        {
+            Timer = true;
+        }
     }
 }
