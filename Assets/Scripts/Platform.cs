6533d7e0c4983274b06027c61ad026f887c48b5c
bukgamedev <bukgamedev@gmail.com>
2024-07-29 21:07:57 +0300

    Movement Of The Hook_Platform object
    Movement of the Hook_Platform object has started.

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    
+    public Rigidbody rb;
+    public float speed;
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        rb.velocity = Vector3.forward * speed;
+    }
 }
