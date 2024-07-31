83028ba83eb0ac828f58b8cf3be01a6a4e617db6
bukgamedev <bukgamedev@gmail.com>
2024-07-31 23:56:17 +0300

    Character Movement and Physical Strength Added to Player 2 Script File
    I made the character move physically correctly with the codes I wrote in the Player 2 script file.

--- Assets/Scripts/Player_2.cs
+++ Assets/Scripts/Player_2.cs
@@ -9,6 +9,7 @@ public class Player_2 : MonoBehaviour
     public GameObject CameraHolder; //Karakterin çocugu olan kamera tutucu objesi.
     public float speed; // Karakterin hareket hýzý.
     public float sensiivity; // Kamera duyarlýlýðý.
+    public float MaxForce; // Kamera duyarlýlýðý.
     private Vector2 move, look; // Hareket ve bakýþ yönlerini tutan vektörler.
     private float LookRotation; //Kamera bakýþ açýsýný tutan deðiþken.
 
@@ -19,7 +20,17 @@ public class Player_2 : MonoBehaviour
     public void OnLook(InputAction.CallbackContext context)
     {
         look = context.ReadValue<Vector2>(); //bakýþ girdisini alýr ve 'look' vektörüne atar.
-}
+    }
+    private void FixedUpdate()
+    {
+        Vector3 CurrentVelocity = rb.velocity; // Karakterin mevcut hýzýný alýr.
+        Vector3 TargetVelocity = new Vector3(move.x,0,move.y); // Hareket girdisinden hedef hýzý oluþturur.
+        TargetVelocity *= speed; // Hedef hýzý karakterin hareket hýzý ile çarpar.
+        TargetVelocity = transform.TransformDirection(TargetVelocity); // Hedef hýzý dünya koordinatlarýna dönüþtürür.
+        Vector3 VelocityChange = (TargetVelocity - CurrentVelocity);// Mevcut hýz ile hedef hýz arasýndaki farký hesaplar.
+        Vector3.ClampMagnitude(VelocityChange, MaxForce);// Hýz deðiþimini maksimum kuvvetle sýnýrlar.
+        rb.AddForce(VelocityChange, ForceMode.VelocityChange);// Rigidbody'ye hýz deðiþimini uygular.
+    }
 
     // Start is called before the first frame update
     void Start()
