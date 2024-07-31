b0c72bfeb001ca7091bfdef5bf2c5fde30cd23e8
bukgamedev <bukgamedev@gmail.com>
2024-07-31 23:46:26 +0300

    Character Components and Input System Codes were Written in the Player 2 Scirpt File
    I added an input system to my Player 2 script file, wrote the components for the character and camera, and wrote the code to receive the inputs.

--- Assets/Scripts/Player_2.cs
+++ Assets/Scripts/Player_2.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_2 : MonoBehaviour
 {
+    public Rigidbody rb; //Karakterin Rigidbody Componenti.
+    public GameObject CameraHolder; //Karakterin çocugu olan kamera tutucu objesi.
+    public float speed; // Karakterin hareket hýzý.
+    public float sensiivity; // Kamera duyarlýlýðý.
+    private Vector2 move, look; // Hareket ve bakýþ yönlerini tutan vektörler.
+    private float LookRotation; //Kamera bakýþ açýsýný tutan deðiþken.
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();// Hareket girdisini alýr ve 'move' vektörüne atar.
+    }
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        look = context.ReadValue<Vector2>(); //bakýþ girdisini alýr ve 'look' vektörüne atar.
+}
+
     // Start is called before the first frame update
     void Start()
     {
