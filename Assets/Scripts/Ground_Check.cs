509e7c734a0b8d114ce2aa9caf9115ff7acb1128
bukgamedev <bukgamedev@gmail.com>
2024-08-08 22:39:54 +0300

    In Ground_Check Script, OnTriggerStay Method is Written in Ground_Check Script.
    In the Ground Check script, the OnTriggerStay method was written to set the status to true as long as the character touches the ground.
    and arrangements were made.

--- Assets/Scripts/Ground_Check.cs
+++ Assets/Scripts/Ground_Check.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Ground_Check : MonoBehaviour
 {
     public Player_2 Player_2; //Player_2 script dosyasýna ulaþmak için.
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)//Karakter zemine temas ediyorsa.
     {
         if (other.gameObject == Player_2.gameObject)
         {
@@ -13,7 +13,15 @@ public class Ground_Check : MonoBehaviour
         }
         Player_2.SetGrounded(true);
     }
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerStay(Collider other)//Karakter zeminde kalmaya devam ediyorsa.
+    {
+        if (other.gameObject == Player_2.gameObject)
+        {
+            return;
+        }
+        Player_2.SetGrounded(true);
+    }
+    private void OnTriggerExit(Collider other)//Karakter zeminden ayrýldýysa.
     {
         if (other.gameObject == Player_2.gameObject)
         {
