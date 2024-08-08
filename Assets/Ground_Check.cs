05e3bc9b4b9fc70965b9786c8e7b1b8e837fc04c
bukgamedev <bukgamedev@gmail.com>
2024-08-08 22:22:46 +0300

    Added OnTriggerEnter Method To Ground_Check Script For Player-Ground Collision Detection
    In the Ground_Check script, the OnTriggerEnter method, which is triggered when the Player_2 object collides with another object, has been implemented.

--- Assets/Ground_Check.cs
+++ Assets/Ground_Check.cs
@@ -1,7 +1,16 @@
-
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Ground_Check : MonoBehaviour
 {
-    
+    public Player_2 Player_2; //Player_2 script dosyasýna ulaþmak için.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player_2.gameObject)
+        {
+            return;
+        }
+        Player_2.SetGrounded(true);
+    }
 }
