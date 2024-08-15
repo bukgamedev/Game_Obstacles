974eeebdb498b397714660080afa92874754d328
bukgamedev <bukgamedev@gmail.com>
2024-08-15 23:25:34 +0300

    Checkpoint System Completed.
    In the KillZone script, if the Character fell down, I wrote code to teleport the Character to the checkpoint position.

--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class KillZone : MonoBehaviour
 {
-    public Vector3 CheckPos;
-    public GameObject Player;
+    public Vector3 CheckPos; //Kontrol noktasý pozisyonu deðiþkeni.
+
+    public GameObject Player; //Oyuncu GameObject'ine referans.
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player") //Oyuncu tagindeki nesnesiyle çarpýþma varsa
         {
-            Player.transform.position = CheckPos;
-            /*
-            Önceki kodlar.
-            other.GetComponent<Health>().Die();
-            Debug.Log("Karakter öldü");*/
+            Rigidbody rb = Player.GetComponent<Rigidbody>(); //  Oyuncunun Rigidbody bileþenini alýr.
+            rb.velocity = Vector3.zero; //  Karakterin hýzýný sýfýrlar.
+            Player.transform.position = CheckPos; //  Karakteri checkpoint pozisyonuna ýþýnlar.
+
+        /*
+        Önceki kodlar.
+        other.GetComponent<Health>().Die();
+        Debug.Log("Karakter öldü");*/
         }
+
+
     }
 }
+
