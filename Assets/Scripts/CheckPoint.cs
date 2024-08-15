b835fa98d41dd9aa429c31696cec8cf3746dc553
bukgamedev <bukgamedev@gmail.com>
2024-08-15 22:26:50 +0300

    KillZone and Checkppoint Checkppoint Script Updated
    The codes in KillZone were updated and the codes were written into the OntriggerEnter function in the CheckPoint script.

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public KillZone deathwall; //Killzone scripti.
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            deathwall.CheckPos = other.transform.position; //deathwalldaki poziyonun pozisyonunu karakterin pozisyonu yapar. 
+        }
     }
 }
--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class KillZone : MonoBehaviour
 {
+    public Vector3 CheckPos;
+    public GameObject Player;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.tag == "Player")
         {
+            Player.transform.position = CheckPos;
+            /*
+            Önceki kodlar.
             other.GetComponent<Health>().Die();
-            Debug.Log("Karakter öldü");
+            Debug.Log("Karakter öldü");*/
         }
     }
 }
