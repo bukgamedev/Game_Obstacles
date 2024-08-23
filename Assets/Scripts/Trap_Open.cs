2da2b3e40a43f7cc924967674e9c6926324a3521
bukgamedev <bukgamedev@gmail.com>
2024-08-23 20:10:43 +0300

    Error Checking in Trap Door Object
    To check the error in the code, I printed whether the console was touched or not.

--- Assets/Scripts/Trap_Open.cs
+++ Assets/Scripts/Trap_Open.cs
@@ -7,6 +7,7 @@ public class Trap_Open : MonoBehaviour
     public GameObject Trap_Door; //Trapdoor objesi
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Tuzaða temas edildi.");
         Trap_Door.GetComponent<Animation>().Play("TrapDoor"); //Trap_Door objesinin Animation componentine eriþ ve TrapDoor adýndaki animasyonu oynat.
     }
 }
