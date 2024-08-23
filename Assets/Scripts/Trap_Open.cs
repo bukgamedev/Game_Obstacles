020b469c68c5db065d61cc607b15eab424640e01
bukgamedev <bukgamedev@gmail.com>
2024-08-23 21:19:07 +0300

    The Parameters in The OnTriggerEnter Function in the Trap_Open Ccript Have Been Deleted.
    The parameters in the OnTriggerEnter function in the Trap_Open script have been deleted. The error message appears again. (Collider other)

--- Assets/Scripts/Trap_Open.cs
+++ Assets/Scripts/Trap_Open.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Trap_Open : MonoBehaviour
 {
     public GameObject Trap_Door; //Trapdoor objesi
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter()
     {
         Debug.Log("Tuzaða temas edildi.");
         Trap_Door.GetComponent<Animation>().Play("TrapDoor"); //Trap_Door objesinin Animation componentine eriþ ve TrapDoor adýndaki animasyonu oynat.
