4cf13688b016abeb63c26a3446f742df46bb908a
bukgamedev <bukgamedev@gmail.com>
2024-09-05 21:25:58 +0300

    Variables in Arrows Script Edited
    I will update the Arrows Script. I started the update from variables

--- Assets/Scripts/Arrows.cs
+++ Assets/Scripts/Arrows.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Arrows : MonoBehaviour
 {
-    public GameObject Arrow; //Ok objesi
+    public GameObject[] Arrow; // Birden fazla ok
+    public float delayBetweenArrows = 0.5f; // Oklar arasýndaki gecikme süresi
+    private bool triggered = false; // Tekrar tetiklenmesini engellemek için
+
     void OnTriggerEnter()
     {
         Debug.Log("Ok Trigger'ýna girildi.");
-        Arrow.GetComponent<Animation>().Play("Arrows");
+        //Arrow[].GetComponent<Animation>().Play("Arrows");
     }
 }
